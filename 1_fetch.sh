ff4e65ba7e16321c03bee51bce0336bb10c35856
gmkung <guangmian@gmail.com>
2024-11-19 23:25:28 +0800

    Fixed the URL to point to the latest curate subgraph, and fixed permission issues in .gitignore

--- 1_fetch.sh
+++ 1_fetch.sh
@@ -6,12 +6,16 @@ if [ -f .env ]; then
   export $(cat .env | xargs)
 fi
 
-API_ENDPOINT="https://gateway-arbitrum.network.thegraph.com/api/${THEGRAPH_API_KEY}/deployments/id/QmevWPSB6PYKWrQCfD52nNtgYoFN4cRA3Dq2MjMjyu9Q9L"
-QUERY='{"query":"{litems(where:{status_in:[Registered], registry:\"0xae6aaed5434244be3699c56e7ebc828194f26dc3\"}){itemID status key0 key1 key2}}"}'
+#API_ENDPOINT="https://gateway-arbitrum.network.thegraph.com/api/${THEGRAPH_API_KEY}/deployments/id/QmevWPSB6PYKWrQCfD52nNtgYoFN4cRA3Dq2MjMjyu9Q9L"
+API_ENDPOINT="https://api.studio.thegraph.com/query/61738/legacy-curate-gnosis/version/latest"
+QUERY='{
+  "query": "{ litems(first: 1000, skip: 0, orderBy: latestRequestSubmissionTime, where: {status: Registered, registryAddress: \"0xae6aaed5434244be3699c56e7ebc828194f26dc3\"}) { itemID metadata { props { type label value } } } }"
+}'
 DATA_FILE="data.json"
 
 # Use curl to execute GraphQL query
 RESPONSE=$(curl -s -X POST -H "Content-Type: application/json" -d "$QUERY" $API_ENDPOINT)
+
 HTTP_CODE=$(echo $RESPONSE | jq -r 'if .errors then "400" else "200" end')
 
 echo "HTTP Status Code: $HTTP_CODE"
@@ -19,6 +23,11 @@ if [[ "$HTTP_CODE" != "200" ]]; then
   echo "Error detected:" $(echo $RESPONSE | jq '.errors[].message') | tee -a error_log.txt
   exit 1
 else
-  PARSED_RESPONSE=$(echo $RESPONSE | jq '[.data.litems[] | {url: .key0, commit: .key1, chainId: .key2}]')
-  echo $PARSED_RESPONSE > $DATA_FILE
+  PARSED_RESPONSE=$(echo $RESPONSE | jq '[.data.litems[] | {
+    itemID: .itemID,
+    url: (.metadata.props[] | select(.label == "Github Repository URL").value),
+    commit: (.metadata.props[] | select(.label == "Commit hash").value),
+    chainId: (.metadata.props[] | select(.label == "EVM Chain ID").value)
+  }]')
+  echo $PARSED_RESPONSE >$DATA_FILE
 fi
